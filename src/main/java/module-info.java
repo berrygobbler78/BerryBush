2918e04a9499dd8d87829c79cb0655b927a7d187
berrygobbler78 <w.garis.2006@gmail.com>
2026-01-29 11:58:15 -0700

    1. Now usese FXThemes to restore functionality of window.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,14 @@ module com.berrygobbler78.flacplayer {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires javafx.graphics;
 
     requires org.controlsfx.controls;
     requires java.desktop;
     requires jflac.codec;
     requires jaudiotagger;
     requires java.xml.crypto;
+    requires com.pixelduke.fxthemes;
     requires com.jfoenix;
 
     opens com.berrygobbler78.flacplayer to javafx.fxml;
