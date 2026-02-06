08e570de24acb97c07ce847608bfe2ea0f60223a
berrygobbler78 <w.garis.2006@gmail.com>
2026-02-06 16:01:28 -0700

    1. Added more images 2. cleaned up functionality with adding variable to enum

--- src/main/java/com/berrygobbler78/flacplayer/Images.java
+++ src/main/java/com/berrygobbler78/flacplayer/Images.java
@@ -6,32 +6,48 @@ import java.util.Objects;
 
 public class Images {
 //    General Icons
-    private static final Image warningImage = new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/warning.png")));
-    private static final Image cdImage = new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/cd.png")));
-    private static final Image songImage = new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/song.png")));
+    private static final Image warningImage =
+        new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/warning.png")));
+    private static final Image cdImage =
+            new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/cd.png")));
+    private static final Image songImage =
+            new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/song.png")));
+    private static final Image userImage =
+            new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/user.png")));
 
-//    Interactables
-    private static final Image playImage = new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/play.png")));
-    private static final Image pauseImage = new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/pause.png")));
+//    Interactable
+    private static final Image playImage =
+        new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/play.png")));
+    private static final Image pauseImage =
+            new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/pause.png")));
 
-    public enum ImageName {
-      WARNING, CD, SONG, PLAY, PAUSE
-    }
+    // Bottom bar
+    private static final Image repeatUnselectedImage =
+            new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/repeat_unselected.png")));
+    private static final Image repeatSelectedImage =
+            new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/repeat_selected.png")));
+    private static final Image repeatOneSelectedImage =
+            new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/repeat_one_selected.png")));
 
-    public static Image getImage(ImageName image) {
-        switch (image) {
-            case WARNING:
-                return warningImage;
-            case CD:
-                return cdImage;
-            case SONG:
-                return songImage;
-            case PLAY:
-                return playImage;
-            case PAUSE:
-                return pauseImage;
-            default:
-                return warningImage;
+    private static final Image shuffleUnselectedImage =
+            new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/shuffle_unselected.png")));
+    private static final Image shuffleSelectedImage =
+            new Image(Objects.requireNonNull(Images.class.getResourceAsStream("/com/berrygobbler78/flacplayer/graphics/shuffle_selected.png")));
+
+
+    public enum IMAGE {
+        WARNING(warningImage), CD(cdImage), SONG(songImage), USER(userImage),
+        PLAY(playImage), PAUSE(pauseImage),
+        REPEAT_UNSELECTED(repeatUnselectedImage), REPEAT_ALL(repeatSelectedImage),REPEAT_ONE(repeatOneSelectedImage),
+        SHUFFLE_UNSELECTED(shuffleUnselectedImage), SHUFFLE_SELECTED(shuffleSelectedImage);
+
+        private Image image;
+        IMAGE(Image image) {
+            this.image = image;
+        };
+
+        public Image get() {
+            return image;
         }
     }
 }
