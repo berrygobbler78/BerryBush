c210a2f3f6966de88c8e5687ec8c5138949cef9f
berrygobbler78 <w.garis.2006@gmail.com>
2026-02-06 16:04:38 -0700

    1. Updated with more info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,7 +11,15 @@ module com.berrygobbler78.flacplayer {
     requires java.xml.crypto;
     requires com.pixelduke.fxthemes;
     requires com.jfoenix;
+    requires com.sun.jna.platform;
+    requires com.sun.jna;
 
     opens com.berrygobbler78.flacplayer to javafx.fxml;
     exports com.berrygobbler78.flacplayer;
+    exports com.berrygobbler78.flacplayer.controllers;
+    opens com.berrygobbler78.flacplayer.controllers to javafx.fxml;
+    exports com.berrygobbler78.flacplayer.util;
+    opens com.berrygobbler78.flacplayer.util to javafx.fxml;
+    exports com.berrygobbler78.flacplayer.userdata;
+    opens com.berrygobbler78.flacplayer.userdata to javafx.fxml;
 }
