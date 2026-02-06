066bebcb46f6d071c2f495606f29fe599c9a0992
berrygobbler78 <w.garis.2006@gmail.com>
2026-02-06 15:59:40 -0700

    1. Consolidated previous enum files to one file

---
+++ src/main/java/com/berrygobbler78/flacplayer/Enums.java
@@ -0,0 +1,28 @@
+package com.berrygobbler78.flacplayer;
+
+public class Enums {
+    public enum PARENT_TYPE {
+        ALBUM,
+        PLAYLIST
+    }
+
+    public enum REPEAT_STATUS {
+        OFF,
+        REPEAT_ALL,
+        REPEAT_ONE
+    }
+
+    public enum FILTER_TYPE {
+        FOLDER,
+        FLAC,
+        COVER_IMAGE,
+        COVER_ICON
+    }
+
+    public enum FILE_TYPE{
+        SONG,
+        ALBUM,
+        ARTIST,
+        PLAYLIST
+    }
+}
