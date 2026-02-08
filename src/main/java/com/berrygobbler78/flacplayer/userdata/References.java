a62798cdd8d92562041763fecc040e61c3a23eff
berrygobbler78 <w.garis.2006@gmail.com>
2026-02-07 20:45:15 -0700

    1. Added logger 2. Cleaned up code

--- src/main/java/com/berrygobbler78/flacplayer/userdata/References.java
+++ src/main/java/com/berrygobbler78/flacplayer/userdata/References.java
@@ -3,9 +3,14 @@ package com.berrygobbler78.flacplayer.userdata;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import com.berrygobbler78.flacplayer.userdata.Playlist;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.berrygobbler78.flacplayer.App;
 
 public class References implements Serializable {
+    private static final Logger LOGGER = Logger.getLogger(References.class.getName());
+
     private String rootDirectoryPath;
     private String userName;
 
@@ -13,6 +18,8 @@ public class References implements Serializable {
 
     public void setUserName(String userName) {
         this.userName = userName;
+
+        LOGGER.log(Level.INFO, "Username set to: [" + userName + "]");
     }
 
     public String getUserName() {
@@ -21,6 +28,8 @@ public class References implements Serializable {
 
     public void setRootDirectoryPath(String rootDirectoryPath) {
         this.rootDirectoryPath = rootDirectoryPath;
+
+        LOGGER.info(String.format("Root directory path set to: [%s]", rootDirectoryPath));
     }
 
     public String getRootDirectoryPath() {
@@ -29,7 +38,15 @@ public class References implements Serializable {
 
     public void addPlaylist(Playlist playlist) {
         playlists.add(playlist);
-        System.out.println("Added playlist " + playlist.getName());
+
+        LOGGER.info(String.format("Added playlist: [%s]", playlist.getName()));
+    }
+
+    public void removePlaylist(Playlist playlist) {
+        playlists.remove(playlist);
+        App.deletePlaylist(playlist);
+
+        LOGGER.info(String.format("Removed playlist: [%s]", playlist.getName()));
     }
 
     public List<Playlist> getPlaylists() {
