0c40380001bf255bb1bd928cc996cec4edc55e99
wgaris <w.garis.2006@gmail.com>
2026-03-09 01:11:05 -0700

    1. broken currently

--- src/main/java/com/berrygobbler78/flacplayer/controllers/PopupWindowsController.java
+++ src/main/java/com/berrygobbler78/flacplayer/controllers/PopupWindowsController.java
@@ -1,29 +1,29 @@
 package com.berrygobbler78.flacplayer.controllers;
 
 import com.berrygobbler78.flacplayer.App;
-import com.berrygobbler78.flacplayer.userdata.Playlist;
+import com.berrygobbler78.flacplayer.configuration.PlaylistDataHandler;
+import com.berrygobbler78.flacplayer.configuration.PlaylistDataHandler.Playlist;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class PopupWindowsController {
-    @FXML
-    private TextField playlistNameField;
+   @FXML
+   private TextField playlistNameField;
 
-    private Stage stage;
-    private MainController controller;
+   private Stage stage;
+   private MainController controller;
 
-    public void setValues(Stage stage, MainController controller) {
-        this.stage = stage;
-        this.controller = controller;
-    }
+   public void setValues(Stage stage, MainController controller) {
+       this.stage = stage;
+       this.controller = controller;
+   }
 
-    @FXML
-    private void enterPlaylistName(){
-        App.getReferences().addPlaylist(new Playlist(playlistNameField.getText()));
-        App.savePlaylists();
-        stage.close();
+   @FXML
+   private void enterPlaylistName(){
+       PlaylistDataHandler.createPlaylist(playlistNameField.getText(), null);
+       stage.close();
 
-        controller.refreshTreeView();
-    }
+       controller.refreshTreeView();
+   }
 }
