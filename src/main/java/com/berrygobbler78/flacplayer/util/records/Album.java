db4cf3095e99182b545eb89d7a1519a9a62677ce
wgaris <w.garis.2006@gmail.com>
2026-03-10 01:22:43 -0700

    1. Fixed recursive hashcode

--- src/main/java/com/berrygobbler78/flacplayer/util/records/Album.java
+++ src/main/java/com/berrygobbler78/flacplayer/util/records/Album.java
@@ -1,5 +1,11 @@
 package com.berrygobbler78.flacplayer.util.records;
 
 import java.util.List;
+import java.util.Objects;
 
-public record Album(String title, Artist artist, List<Song> songs, String iconPath, String imagePath) { }
+public record Album(String title, Artist artist, List<Song> songs, String iconPath, String imagePath) {
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, iconPath, imagePath);
+    }
+}
