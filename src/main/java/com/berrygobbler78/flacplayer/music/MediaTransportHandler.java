86bef0c2c366390e05e2fe6f45ef84a7e2261fa3
wgaris <w.garis.2006@gmail.com>
2026-04-10 00:25:53 -0700

    1. Set enabled was always true no matter input 2. Uses executor service from App.java

--- src/main/java/com/berrygobbler78/flacplayer/music/MediaTransportHandler.java
+++ src/main/java/com/berrygobbler78/flacplayer/music/MediaTransportHandler.java
@@ -1,22 +1,18 @@
 package com.berrygobbler78.flacplayer.music;
 
+import com.berrygobbler78.flacplayer.App;
 import io.github.selemba1000.*;
-import javafx.concurrent.Task;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class MediaTransportHandler {
 
     private final JMTC jmtc;
     private String currentSong = "";
     private Path currentArtwork;
-
-    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public MediaTransportHandler(String playerName, String playerPath, PlaybackManager playbackManager, QueueManager queueManager) {
         jmtc = JMTC.getInstance(new JMTCSettings(playerName, playerPath));
@@ -41,57 +37,50 @@ public class MediaTransportHandler {
     }
 
     public void setEnabled(boolean enabled) {
-        if(enabled) {
-            jmtc.setEnabled(true);
-            jmtc.setEnabledButtons(new JMTCEnabledButtons(true, true, true, true, true));
-            jmtc.updateDisplay();
-        }
+        jmtc.setEnabled(enabled);
+        jmtc.setEnabledButtons(new JMTCEnabledButtons(enabled, enabled, enabled, enabled, enabled));
+        jmtc.updateDisplay();
     }
 
-    public void setProperties(String songTitle, String songArtist,
-                              String parentTitle, String parentArtist,
-                              int tracks, int index, File coverArt) {
+    public void setProperties(
+            String songTitle, String songArtist, String parentTitle, String parentArtist,
+            int tracks, int index, File coverArt)
+    {
 
-        if(jmtc.getEnabled() == false) setEnabled(true);
+        if(!jmtc.getEnabled()) setEnabled(true);
 
-        Task<Void> update = new Task<>() {
-            @Override
-            protected Void call() throws Exception {
-                File artworkFile = null;
+        App.submitTask(() -> {
+            File artworkFile = null;
 
-                if (coverArt != null && !songTitle.equals(currentSong)) {
-                    currentSong = songTitle;
+            if (coverArt != null && !songTitle.equals(currentSong)) {
+                currentSong = songTitle;
 
-                    // Create a temp file for artwork
-                    File temp = File.createTempFile("currentArt", ".tmp");
-                    temp.deleteOnExit(); // automatically clean up on exit
+                File temp = File.createTempFile("currentArt", ".tmp");
+                temp.deleteOnExit();
 
-                    Files.copy(coverArt.toPath(), temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                    currentArtwork = temp.toPath();
-                }
-
-                if (currentArtwork != null) {
-                    artworkFile = currentArtwork.toFile();
-                }
-
-                jmtc.setMediaProperties(new JMTCMusicProperties(
-                        songTitle,
-                        songArtist,
-                        parentTitle,
-                        parentArtist,
-                        new String[]{},  //  TODO: Add genres
-                        tracks,
-                        index,
-                        artworkFile
-                ));
-                jmtc.updateDisplay();
-
-                return null;
+                Files.copy(coverArt.toPath(), temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                currentArtwork = temp.toPath();
             }
-        };
 
-        executor.submit(update);
-        jmtc.updateDisplay();
+            if (currentArtwork != null) {
+                artworkFile = currentArtwork.toFile();
+            }
+
+            jmtc.setMediaProperties(new JMTCMusicProperties(
+                    songTitle,
+                    songArtist,
+                    parentTitle,
+                    parentArtist,
+                    new String[]{},  //  TODO: Add genres
+                    tracks,
+                    index,
+                    artworkFile
+            ));
+
+            jmtc.updateDisplay();
+
+            return null;
+        });
     }
 
     public void setTimeline(long start, long end, long seekStart, long seekEnd) {
@@ -102,9 +91,5 @@ public class MediaTransportHandler {
     public void setState(JMTCPlayingState state) {
         jmtc.setPlayingState(state);
         jmtc.updateDisplay();
-    }
-
-    public static void shutdown() {
-        executor.shutdown();
     }
 }
