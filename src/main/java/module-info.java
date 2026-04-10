05e9320cb6aacadaafd3841d079148c2d6bd9df5
wgaris <w.garis.2006@gmail.com>
2026-04-10 00:26:04 -0700

    1. Fixed due to refactoring packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -21,18 +21,22 @@ module com.berrygobbler.flacplayer {
     requires jdk.compiler;
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
+    requires java.sql;
 
 
     opens com.berrygobbler78.flacplayer.configuration to com.electronwill.nightconfig.core;
     opens com.berrygobbler78.flacplayer to javafx.fxml;
-    opens com.berrygobbler78.flacplayer.controllers to javafx.fxml;
+    opens com.berrygobbler78.flacplayer.gui.controllers to javafx.fxml;
     exports com.berrygobbler78.flacplayer;
-    exports com.berrygobbler78.flacplayer.controllers to javafx.fxml;
-    opens com.berrygobbler78.flacplayer.util.handlers to javafx.fxml;
+    exports com.berrygobbler78.flacplayer.gui.controllers to javafx.fxml;
     exports com.berrygobbler78.flacplayer.util;
     opens com.berrygobbler78.flacplayer.util to javafx.fxml;
-    exports com.berrygobbler78.flacplayer.util.handlers;
     exports com.berrygobbler78.flacplayer.util.records;
     exports com.berrygobbler78.flacplayer.music;
     opens com.berrygobbler78.flacplayer.music to javafx.fxml;
+    exports com.berrygobbler78.flacplayer.gui.managers;
+    opens com.berrygobbler78.flacplayer.gui.managers to javafx.fxml;
+    exports com.berrygobbler78.flacplayer.gui;
+    opens com.berrygobbler78.flacplayer.gui to javafx.fxml;
+    opens com.berrygobbler78.flacplayer.util.records to javafx.fxml;
 }
