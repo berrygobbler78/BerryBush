a2a7f566b6257d3e23c27125d423b8b7fae7c905
wgaris <w.garis.2006@gmail.com>
2026-04-10 00:23:51 -0700

    1. Prettied code with var

---
+++ src/main/java/com/berrygobbler78/flacplayer/gui/controllers/GenericItemController.java
@@ -0,0 +1,61 @@
+package com.berrygobbler78.flacplayer.gui.controllers;
+
+import com.berrygobbler78.flacplayer.App;
+import com.berrygobbler78.flacplayer.util.ImageUtils;
+import com.berrygobbler78.flacplayer.util.records.Album;
+import com.berrygobbler78.flacplayer.util.records.Artist;
+import com.berrygobbler78.flacplayer.util.records.Song;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class GenericItemController {
+    @FXML
+    private VBox mainVBox;
+    @FXML
+    private ImageView itemImage;
+    @FXML
+    private Label title, subtitle;
+
+    @FXML
+    private void initialize() {
+        mainVBox.getStylesheets().add(String.valueOf(App.class.getResource("css/generic-item.css")));
+        itemImage.setPreserveRatio(true);
+    }
+
+    public void bindHeightProperty(ReadOnlyDoubleProperty property) {
+        itemImage.fitHeightProperty().bind(property.divide(4.5));
+        itemImage.fitWidthProperty().bind(itemImage.fitHeightProperty());
+        mainVBox.maxWidthProperty().bind(itemImage.fitWidthProperty().add(20));
+    }
+
+    public void setItemData(Artist artist) {
+        title.setText(artist.title());
+        subtitle.setText(artist.albums().size() + " albums");
+        itemImage.setImage(
+                (ImageUtils.pathToImage(artist.artPath()) == null) ?
+                        ImageUtils.pathToImage(artist.albums().getFirst().artPath()) :
+                        ImageUtils.pathToImage(artist.artPath()));
+        itemImage.setFitHeight(200);
+        itemImage.setFitWidth(200);
+
+    }
+
+    public void setItemData(Album album) {
+        title.setText(album.title());
+        subtitle.setText(album.artist().title());
+        itemImage.setImage(ImageUtils.pathToImage(album.artPath()));
+        itemImage.setFitHeight(200);
+        itemImage.setFitWidth(200);
+    }
+
+    public void setItemData(Song song) {
+        title.setText(song.title());
+        subtitle.setText(song.album().artist().title());
+        itemImage.setImage(ImageUtils.pathToImage(song.album().artPath()));
+        itemImage.setFitHeight(200);
+        itemImage.setFitWidth(200);
+    }
+}
