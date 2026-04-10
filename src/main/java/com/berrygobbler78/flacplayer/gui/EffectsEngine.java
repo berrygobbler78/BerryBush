9152e72579b978c9199f8471c251a5eb6b8c52d6
wgaris <w.garis.2006@gmail.com>
2026-04-10 00:22:43 -0700

    1. Refactored some code to make prettier 2. Platform.runLater() should make transitions smoother

--- src/main/java/com/berrygobbler78/flacplayer/gui/EffectsEngine.java
+++ src/main/java/com/berrygobbler78/flacplayer/gui/EffectsEngine.java
@@ -1,10 +1,7 @@
-package com.berrygobbler78.flacplayer;
+package com.berrygobbler78.flacplayer.gui;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.ParallelTransition;
 import javafx.animation.ScaleTransition;
-import javafx.animation.TranslateTransition;
-import javafx.scene.Node;
+import javafx.application.Platform;
 import javafx.scene.control.Tab;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
@@ -16,7 +13,7 @@ public class EffectsEngine {
     private static final ColorAdjust EMPTY_TINT = new ColorAdjust(0, 0, 0, 0);
 
     public static void setPressedEffect(ImageView imageView) {
-        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(100), imageView);
+        var scaleTransition = new ScaleTransition(Duration.millis(100), imageView);
 
         imageView.setOnMousePressed(_ -> {
             imageView.setEffect(GREY_TINT);
@@ -34,36 +31,20 @@ public class EffectsEngine {
     }
 
     public static void setPressedEffect(Tab tab) {
-        ImageView imageView = (ImageView) tab.getGraphic();
-        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(250), imageView);
+        var imageView = (ImageView) tab.getGraphic();
+        var scaleTransition = new ScaleTransition(Duration.millis(250), imageView);
 
-        tab.selectedProperty().addListener((_, _, newValue) -> {
+        Platform.runLater(() -> tab.selectedProperty().addListener((_, _, newValue) -> {
             if(newValue) {
                 scaleTransition.setToX(0.95);
                 scaleTransition.setToY(0.95);
                 scaleTransition.playFromStart();
-
                 scaleTransition.setOnFinished(_ -> {
-                    scaleTransition.setToX(1);
-                    scaleTransition.setToY(1);
-                    scaleTransition.playFromStart();
-                });
-            }
-        });
-    }
 
-    public static void slideInContent(Node content) {
-        // Start from the right (e.g., 100 pixels offset)
-        content.setTranslateX(100);
-        content.setOpacity(0);
-
-        var tt = new TranslateTransition(Duration.millis(300), content);
-        tt.setToX(0);
-
-        var ft = new FadeTransition(Duration.millis(300), content);
-        ft.setToValue(1);
-
-        var pt = new ParallelTransition(tt, ft);
-        pt.play();
+                scaleTransition.setToX(1);
+                scaleTransition.setToY(1);
+                scaleTransition.playFromStart();
+            });
+        }}));
     }
 }
