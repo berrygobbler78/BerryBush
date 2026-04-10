069c64f87c8faa4dae69cb26e8eaa2b7fe0b7754
wgaris <w.garis.2006@gmail.com>
2026-04-10 00:21:50 -0700

    1. Added executor service for threading 2. Refactored some code to make prettier

--- src/main/java/com/berrygobbler78/flacplayer/App.java
+++ src/main/java/com/berrygobbler78/flacplayer/App.java
@@ -2,15 +2,16 @@ package com.berrygobbler78.flacplayer;
 
 import java.io.*;
 import java.net.URISyntaxException;
+import java.util.concurrent.*;
+import java.util.logging.Level;
 
 import com.berrygobbler78.flacplayer.configuration.UserDataHandler;
-import com.berrygobbler78.flacplayer.controllers.SetupWizardController;
+import com.berrygobbler78.flacplayer.gui.controllers.SetupWizardController;
 import com.berrygobbler78.flacplayer.music.MusicInterface;
 
 import com.berrygobbler78.flacplayer.util.ImageUtils;
-import com.berrygobbler78.flacplayer.music.MediaTransportHandler;
-import com.berrygobbler78.flacplayer.util.handlers.ResourceHandler;
-import com.berrygobbler78.flacplayer.util.handlers.RecordHandler;
+import com.berrygobbler78.flacplayer.util.ResourceHandler;
+import com.berrygobbler78.flacplayer.util.records.RecordHandler;
 import com.pixelduke.window.ThemeWindowManagerFactory;
 import com.pixelduke.window.Win11ThemeWindowManager;
 import javafx.application.Application;
@@ -25,8 +26,6 @@ import org.apache.logging.log4j.Logger;
 public class App extends Application {
     private static final Logger logger = LogManager.getLogger();
 
-    private static Stage primaryStage;
-
     public enum OS {
         LINUX,
         WINDOWS_10,
@@ -38,21 +37,24 @@ public class App extends Application {
 
     private static MusicInterface musicInterface;
 
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(Thread.getAllStackTraces().size());
+
     @Override
     public void start(Stage stage) throws IOException, URISyntaxException {
-        java.util.logging.Logger.getLogger("org.jaudiotagger").setLevel(java.util.logging.Level.SEVERE);
+        java.util.logging.Logger.getLogger("org.jaudiotagger").setLevel(Level.SEVERE);
 
-        switch (System.getProperty("os.name")) {
-            case "Linux" -> currentOS = OS.LINUX;
-            case "Windows 10" -> currentOS = OS.WINDOWS_10;
-            case "Windows 11" -> currentOS = OS.WINDOWS_11;
-            case "Mac" -> currentOS = OS.MAC;
-        }
+         currentOS = switch (System.getProperty("os.name")) {
+            case "Linux" -> OS.LINUX;
+            case "Windows 10" -> OS.WINDOWS_10;
+            case "Windows 11" -> OS.WINDOWS_11;
+            case "Mac" -> OS.MAC;
+            default -> throw new IllegalStateException("Unexpected value: " + System.getProperty("os.title"));
+         };
 
         ResourceHandler.initialize();
         UserDataHandler.initialize();
 
-        if(UserDataHandler.getPath().equals(File.separator)  || UserDataHandler.getUsername().equals(".")) new SetupWizardController(stage);
+        if(UserDataHandler.getConfig(UserDataHandler.ConfigLocation.PATH).equals(File.separator)  || UserDataHandler.getConfig(UserDataHandler.ConfigLocation.USERNAME).equals(".")) new SetupWizardController(stage);
 
         RecordHandler.cache();
         ImageUtils.refreshAllArt(false);
@@ -62,31 +64,30 @@ public class App extends Application {
 
     private void setupStage(Stage stage) throws IOException {
         logger.info("Initializing...");
-        FXMLLoader fxmlLoader = new FXMLLoader(ResourceHandler.getResourceURL("fxml/landing.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        var fxmlLoader = new FXMLLoader(App.class.getResource("fxml/landing.fxml"));
+        var scene = new Scene(fxmlLoader.load());
         musicInterface = new MusicInterface(fxmlLoader.getController());
-        scene.getStylesheets().add(ResourceHandler.getResourceURL("css/landing.css").toExternalForm());
-        scene.getStylesheets().add(ResourceHandler.getResourceURL("css/bottom-bar.css").toExternalForm());
+        scene.getStylesheets().add(String.valueOf(App.class.getResource("css/landing.css")));
+        scene.getStylesheets().add(String.valueOf(App.class.getResource("css/bottom-bar.css")));
 
-        primaryStage = stage;
-        primaryStage.setTitle("BerryBush");
-        primaryStage.setScene(scene);
-        primaryStage.getIcons().add(new Image(ResourceHandler.getResourceURL("graphics/berries.png").toString()));
-        primaryStage.show();
-        primaryStage.setOnCloseRequest(_ -> MediaTransportHandler.shutdown());
+        stage.setTitle("BerryBush");
+        stage.setScene(scene);
+        stage.getIcons().add(new Image(String.valueOf(App.class.getResource("graphics/berries.png"))));
+        stage.show();
+        stage.setOnCloseRequest(_ -> executorService.shutdown());
+
 
         if(currentOS == OS.WINDOWS_11) {
-            Win11ThemeWindowManager themeWindowManager =
-                    (Win11ThemeWindowManager) ThemeWindowManagerFactory.create(); // For coloring the window border
-            themeWindowManager.setWindowFrameColor(primaryStage, Color.web("#121212"));
-            themeWindowManager.setDarkModeForWindowFrame(primaryStage, true);
+            var themeWindowManager = (Win11ThemeWindowManager) ThemeWindowManagerFactory.create();
+            themeWindowManager.setWindowFrameColor(stage, Color.web("#121212"));
+            themeWindowManager.setDarkModeForWindowFrame(stage, true);
         }
 
         logger.info("Stage created.");
     }
 
-    public static Stage getPrimaryStage() {
-        return primaryStage;
+    public static void submitTask(Callable<?> task) {
+        executorService.submit(task);
     }
 
     public static OS getOS() {
