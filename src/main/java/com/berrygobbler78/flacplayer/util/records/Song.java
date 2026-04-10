c1f243b72ab2f1782a8c6342dc7bdd471c44c657
wgaris <w.garis.2006@gmail.com>
2026-04-10 00:31:01 -0700

    1. Cleaned up code for understandability

--- src/main/java/com/berrygobbler78/flacplayer/util/records/Song.java
+++ src/main/java/com/berrygobbler78/flacplayer/util/records/Song.java
@@ -2,12 +2,12 @@ package com.berrygobbler78.flacplayer.util.records;
 
 import java.util.Objects;
 
-public record Song(String title, Album album, Artist artist, int track, int disc, String path) {
+public record Song(String title, Album album, int track, int disc, String path) {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Song song = (Song) o;
+        var song = (Song) o;
         return track == song.track &&
                Objects.equals(title, song.title) &&
                Objects.equals(path, song.path);
