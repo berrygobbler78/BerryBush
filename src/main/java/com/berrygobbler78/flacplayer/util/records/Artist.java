5683df7272ede73d34abde3ae503a1e04d2035db
wgaris <w.garis.2006@gmail.com>
2026-04-10 00:22:08 -0700

    1. Artist.name() now Artist.title()

--- src/main/java/com/berrygobbler78/flacplayer/util/records/Artist.java
+++ src/main/java/com/berrygobbler78/flacplayer/util/records/Artist.java
@@ -3,18 +3,18 @@ package com.berrygobbler78.flacplayer.util.records;
 import java.util.List;
 import java.util.Objects;
 
-public record Artist(String name, List<Album> albums, String artPath) {
+public record Artist(String title, List<Album> albums, String artPath) {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Artist artist = (Artist) o;
-        return Objects.equals(name, artist.name) &&
+        var artist = (Artist) o;
+        return Objects.equals(title, artist.title) &&
                Objects.equals(artPath, artist.artPath);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, artPath);
+        return Objects.hash(title, artPath);
     }
 }
