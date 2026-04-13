28249e5a6db655e6360d416a1fc13dcddd065e4f
wgaris <w.garis.2006@gmail.com>
2026-04-13 13:50:35 -0700

    1. Now uses null-safe optional for flac-to-wav return

--- src/main/java/com/berrygobbler78/flacplayer/util/FlacDecoder.java
+++ src/main/java/com/berrygobbler78/flacplayer/util/FlacDecoder.java
@@ -8,24 +8,33 @@ import org.jflac.metadata.StreamInfo;
 import org.jflac.util.ByteData;
 import org.jflac.util.WavWriter;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
 
 public class FlacDecoder implements PCMProcessor {
     private final Logger logger = LogManager.getLogger();
 
     private WavWriter wav;
 
-    public void flacToWav(String inPath, String outPath) {
-        try (var is = new FileInputStream(inPath); var os = new FileOutputStream(outPath)) {
+    public Optional<Path> flacToWav(String source) throws IOException {
+        var tempFile =
+                File.createTempFile("temp", ".wav", ResourceHandler.get(ResourceHandler.ResourceType.TEMP));
+
+        try (var is = new FileInputStream(source); var os = new FileOutputStream(tempFile.getPath())) {
             wav = new WavWriter(os);
 
             var flacDecoder = new FLACDecoder(is);
             flacDecoder.addPCMProcessor(this);
             flacDecoder.decode();
+
+            return Optional.of(tempFile.toPath());
         } catch (IOException e) {
-            logger.error("Error while converting '{}' : {}", inPath, e.getMessage());
+            logger.error("Error while converting '{}' | {}", source, e.getMessage());
+            return Optional.empty();
         }
     }
 
